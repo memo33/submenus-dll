24402c2d23e3834aa6b21eb0d7b6c5f5f04b601d
memo <memo33@users.noreply.github.com>
2025-07-16 19:46:22 +0200

    bump version to 1.1.5

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define PLUGIN_VERSION_STR      "1.1.4"
-#define RESOURCE_VERSION         1,1,4,0
-#define RESOURCE_VERSION_STR    "1.1.4.0"
+#define PLUGIN_VERSION_STR      "1.1.5"
+#define RESOURCE_VERSION         1,1,5,0
+#define RESOURCE_VERSION_STR    "1.1.5.0"
